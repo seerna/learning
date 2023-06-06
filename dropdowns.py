8b2d0df5554f97588b81b7d00ebc05a184a34d9d
senzen <sant@Blep.local>
2023-06-06 00:00:29 -0500

    commiting idk why

--- dropdowns.py
+++ dropdowns.py
@@ -10,12 +10,18 @@ def clicked():
     global selection
     selection = Label(root, text=dropdown.get()).pack()
 
-options = ['prick this', 'Not! Tis', 'Oh yeah here', 'keep it']
+options = [
+    'prick this',
+    'Not! Tis',
+    'Oh yeah here',
+    'keep it',
+    'add stuff'
+    ]
 
 dropdown = StringVar()
 dropdown.set(options[0])
 
-# Note the *, it-s done so options is taken as items and not an entire list
+# Note the *, it's done so options is taken as items and not an entire list
 omg_drop = OptionMenu(root, dropdown, *options)
 omg_drop.pack()
 
