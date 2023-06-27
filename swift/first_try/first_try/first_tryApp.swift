054d140965624a9356da1f8d701b80d90231fb67
senzen <san.serna12@gmail.com>
2023-06-26 22:03:20 -0500

    added a swift project, gonna play a bit

---
+++ swift/first_try/first_try/first_tryApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_tryApp.swift
+//  first_try
+//
+//  Created by Santiago on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct first_tryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
